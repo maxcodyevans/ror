a4fc180d838c8393af03aa47396bad8405d4f6bf
Max <maxcodyevans@gmail.com>
2015-06-19 15:59:37 -0500

    test2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
+  
   get 'welcome/about'
   root to: 'welcome#about'
 
