34c63c270f035eeb6c95f9aa3774765ad3c93a83
Max <maxcodyevans@gmail.com>
2015-06-20 00:30:00 -0500

    undo shits

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,7 +37,7 @@ class User < ActiveRecord::Base
           email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
           password: Devise.friendly_token[0,20]
         )
-        user.skip_confirmation!
+        #user.skip_confirmation!
         user.create!
 #previously user save
       end
