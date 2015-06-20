d9db913cd97d6b76d75d32508e849c4cedd4a559
Max <maxcodyevans@gmail.com>
2015-06-19 20:39:28 -0500

    privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   
   get 'welcome/about'
   root to: 'welcome#about'
+  
+   match '/privacypolicy', :to => 'welcome#privacypolicy',          via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
