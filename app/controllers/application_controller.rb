a48cc3e53d74f4d8f9c26bf2ccbc362b2878cbd5
Max <maxcodyevans@gmail.com>
2015-06-20 09:37:41 -0500

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,9 @@ class ApplicationController < ActionController::Base
     end
   end
   
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || edit_user_registration_path
+  end
+end
+  
 end
