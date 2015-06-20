1ee3b9b9aa9efecf9c7a4ef0b97ad8af8613d847
Max <maxcodyevans@gmail.com>
2015-06-20 09:42:24 -0500

    end too mcuh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,6 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || edit_user_registration_path
   end
-end
+
   
 end
